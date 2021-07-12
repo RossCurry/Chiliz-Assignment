f3ed2d68ed22a82c751bfefe0f9c3dda2c0c0edb
Olga Svishcheva <olgasvishcheva@Olgas-MacBook-Air.local>
2021-07-12 21:15:49 +0200

    challenge finished in 2hrs25mins

--- main.js
+++ main.js
@@ -28,7 +28,92 @@ const dates = [
     ['17.02.2024', '17.02.2025'],
 ];
 
+const months = {
+    "01": 'January',
+    "02": 'February',
+    "03": 'March',
+    "04": 'April',
+    "05": 'May',
+    "06": 'June',
+    "07": 'July',
+    "08": 'August',
+    "09": 'September',
+    "10": 'Octuber',
+    "11": 'November',
+    "12": 'December',
+}
+
 // Receive string of dates one after each other
 function outputDate(dates) {
-    return null;
+    const dateArrInMilliseconds = [];
+    const dayArr = [];
+    const monthArr = [];
+    const yearArr = [];
+    for (let i = 0; i < dates.length; i++) {
+        const date = dates[i];
+
+        const miliseconds = convertToMilliseconds(date);
+        dateArrInMilliseconds.push(miliseconds);
+        
+        const [days, months, years] = divideDates(date);
+        dayArr.push(days);
+        monthArr.push(months);
+        yearArr.push(years);
+
+    }
+    const differenceInMilliseconds = dateArrInMilliseconds[1] - dateArrInMilliseconds[0];
+    const months = calculateMonthDifference(dayArr, monthArr, yearArr);
+    const [days, years] = calculateDaysAndYears(differenceInMilliseconds);
+    const yearString = years ? `${years} year${pluralOrSingular(years)}, ` : '';
+    const monthString = months ? `${months} month${pluralOrSingular(months)}, ` : '';
+    const dayString = days ? `${days} day${pluralOrSingular(days)}` : '0 days';
+    const result = `${yearString}${monthString}total ${dayString}`;
+    return result;
+}
+
+function calculateDaysAndYears(milliseconds) {
+    const seconds = milliseconds/1000;
+    const minutes = seconds/60;
+    const hours = minutes/60;
+    const days = Math.ceil(hours/24);
+    const years = Math.floor(days/365);
+    return [days, years, months];
+}
+
+
+function calculateMonthDifference(dayArr, monthArr, yearArr) {
+    const dayDiff = dayArr[1] - dayArr[0];
+    const yearDiff = yearArr[1] - yearArr[0];
+    if (yearDiff === 0) {
+        return monthArr[1] - monthArr[0];
+    } else if  (yearDiff > 0 && monthArr[1] >= monthArr[0]) {
+        const difference = monthArr[1] - monthArr[0];
+        if (dayDiff < 0) return difference - 1;
+        else return difference;
+    } else if  (yearDiff > 0 && monthArr[1] < monthArr[0]) {
+        const difference = monthArr[1] + 12 - monthArr[0];
+        if (dayDiff < 0) return difference - 1;
+        else return difference;
+    }
+}
+
+// helper functions
+function convertToMilliseconds(ddmmyyyy) {
+    const dividedDates = ddmmyyyy.split('.');
+    dividedDates[1] = months[dividedDates[1]];
+    const joinedDates = dividedDates.join(' ');
+    const dateObj = new Date(joinedDates);
+    const dateInMillis = dateObj.valueOf();
+    return dateInMillis;
+}
+
+function divideDates(date) {
+    const dividedDay = date.split('.')[0];
+    const dividedMonth = date.split('.')[1];
+    const dividedYear = date.split('.')[2];
+    return [+dividedDay, +dividedMonth, +dividedYear];
+}
+
+function pluralOrSingular(number) {
+    return number > 1 ? 's' : '';
 }
